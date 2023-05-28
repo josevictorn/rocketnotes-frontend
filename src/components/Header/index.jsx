b940d6e4705dfadd9b77a978ad05b2dce74976d7
victor <josevictornascimento2016@gmail.com>
2023-05-28 12:42:16 -0300

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,44 @@
+import { useAuth } from '../../hooks/auth';
+import { useNavigate } from 'react-router-dom';
+
+import { api } from "../../services";
+
+import { Container, Profile, Logout } from './styles';
+import { RiShutDownLine } from 'react-icons/ri';
+
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg';
+
+export function Header() {
+  const { signOut, user } = useAuth();
+
+  const navigate = useNavigate();
+
+  function handleSignOut() {
+    navigate("/");
+    signOut();
+  }
+
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
+
+  return(
+    <Container>
+      <Profile to="/profile">
+        <img 
+          src={avatarUrl}
+          alt={user.name}
+        />
+
+        <div>
+          <span>Bem-vindo,</span>
+          <strong>{user.name}</strong>
+        </div>
+      </Profile>
+
+      <Logout
+        onClick={handleSignOut}
+      >
+        <RiShutDownLine />
+      </Logout>
+    </Container>
+  )
+}
---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,107 @@
+import { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+
+import { api } from '../../services';
+
+import { Container, Links, Content } from './styles';
+
+import { Header } from '../../components/Header';
+import { Button } from '../../components/Button';
+import { Section } from '../../components/Section';
+import { Tag } from '../../components/Tag';
+import { ButtonText } from '../../components/ButtonText';
+
+export function Details() {
+  const [data, setData] = useState(null);
+
+  const params = useParams();
+  const navigate = useNavigate();
+
+  function handleBack() {
+    navigate(-1);
+  }
+
+  async function handleRemove() {
+    const isConfirm = window.confirm("Tem certeza que deseja deletar essa nota?");
+
+    if(isConfirm) {
+      await api.delete(`/notes/${params.id}`);
+      navigate(-1);
+    }
+  }
+
+  useEffect(() => {
+    async function fetchNote() {
+      const response = await api.get(`/notes/${params.id}`);
+      setData(response.data);
+    }
+
+    fetchNote();
+  }, []);
+
+  return (
+    <Container>
+      <Header />
+
+      {
+        data &&
+        <main>
+          <Content>
+            <ButtonText 
+              title="Excluir nota" 
+              onClick={handleRemove}
+            />
+
+            <h1>
+              {data.title}
+            </h1>
+
+            <p>
+              {data.description}
+            </p>
+
+            {
+              data.links &&
+              <Section title="Links úteis">
+                <Links>
+                  {
+                    data.links.map(link => (
+                      <li key={link.id}>
+                        <a 
+                          href={link.url}
+                          target="_blank"
+                        >
+                          {link.url}
+                        </a>
+                      </li>
+                    ))
+                  }
+                </Links>
+              </Section>
+            }
+
+            {
+              data.tags &&
+              <Section title="Marcadores">
+                {
+                  data.tags.map(tag => (
+                    <Tag 
+                      key={tag.id}
+                      title={tag.name} 
+                    />
+                  ))
+                }
+              </Section>
+            }
+
+            <Button
+              title="Voltar" 
+              onClick={handleBack}
+            />
+          </Content>
+        </main>
+      }
+
+    </Container>
+  )
+}
